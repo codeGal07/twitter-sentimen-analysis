abe16f430d95c1d33b8cb27f3bc038ffd56cc3d9
sabinary <sabina.matjasic@gmail.com>
2022-01-04 21:53:05 +0100

    convert emoji unicode to words for sentimental analysis

--- TwitterSentimentAnalysis.py
+++ TwitterSentimentAnalysis.py
@@ -1,6 +1,7 @@
 import tweepy as tweepy
 import csv
 import datetime
+import pandas as pd
 
 
 def get_twitter_data(username, startDate, endDate):
@@ -47,11 +48,81 @@ def get_twitter_data(username, startDate, endDate):
     pass
 
 
+def edit_text(text):
+    text = text.replace("\xf0\x9f\x98\x80", "happy ")
+    text = text.replace("\xf0\x9f\x98\x87", "smile ")
+    text = text.replace("\xF0\x9F\x98\x88", "evil happy ")
+    text = text.replace("\xF0\x9F\x98\x8E", "cool ")
+    text = text.replace("\xF0\x9F\x98\x90", "neutral ")
+    text = text.replace("\xF0\x9F\x98\x91", "expressionless ")
+    text = text.replace("\xF0\x9F\x98\x95", "confused ")
+    text = text.replace("\xF0\x9F\x98\x97", "kissing ")
+    text = text.replace("\xF0\x9F\x98\x99", "kissing ")
+    text = text.replace("\xF0\x9F\x98\x9B", "funny ")
+    text = text.replace("\xF0\x9F\x98\x9F", "worried ")
+    text = text.replace("\xF0\x9F\x98\xA6", "frowning ")
+    text = text.replace("\xF0\x9F\x98\xA7", "anguished ")
+    text = text.replace("\xF0\x9F\x98\xAC", "grimacing ")
+    text = text.replace("\xF0\x9F\x98\xAE", "shoked ")
+    text = text.replace("\xF0\x9F\x98\xAF", "hushed ")
+    text = text.replace("\xF0\x9F\x98\xB4", "sleeping ")
+    text = text.replace("\xF0\x9F\x98\xB6", "silent ")
+    text = text.replace("\xF0\x9F\x98\x81", "smile ")
+    text = text.replace("\xF0\x9F\x98\x82", "hilarious ")
+    text = text.replace("\xF0\x9F\x98\x83", "happy ")
+    text = text.replace("\xF0\x9F\x98\x84", "happy ")
+    text = text.replace("\xF0\x9F\x98\x85", "happy Sweat ")
+    text = text.replace("\xF0\x9F\x98\x86", "grinning ")
+    text = text.replace("\xF0\x9F\x98\x89", "wink happy ")
+    text = text.replace("\xF0\x9F\x98\x8A", "smiling ")
+    text = text.replace("\xF0\x9F\x98\x8B", "savouring  ")
+    text = text.replace("\xF0\x9F\x98\x8C", "relieved ")
+    text = text.replace("\xF0\x9F\x98\x8D", "love ")
+    text = text.replace("\xF0\x9F\x98\x8F", "smirking ")
+    text = text.replace("\xF0\x9F\x98\x92", "unamused ")
+    text = text.replace("\xF0\x9F\x98\x93", "hard work ")
+    text = text.replace("\xF0\x9F\x98\x94", "sad ")
+    text = text.replace("\xF0\x9F\x98\x96", "confounded ")
+    text = text.replace("\xF0\x9F\x98\x98", "kissing love ")
+    text = text.replace("\xF0\x9F\x98\x9A", "kiss ")
+    text = text.replace("\xF0\x9F\x98\x9C", "joking ")
+    text = text.replace("\xF0\x9F\x98\x9D", "joking ")
+    text = text.replace("\xF0\x9F\x98\x9E", "disappointed ")
+    text = text.replace("\xF0\x9F\x98\xA0", "angry ")
+    text = text.replace("\xF0\x9F\x98\xA1", "furious ")
+    text = text.replace("\xF0\x9F\x98\xA2", "crying ")
+    text = text.replace("\xF0\x9F\x98\xA3", "helpless ")
+    text = text.replace("\xF0\x9F\x98\xA4", "frustrated ")
+    text = text.replace("\xF0\x9F\x98\xA5", "disappointed ")
+    text = text.replace("\xF0\x9F\x98\xA8", "fearful ")
+    text = text.replace("\xF0\x9F\x98\xA9", "weary ")
+    text = text.replace("\xF0\x9F\x98\xAA", "sleepy ")
+    text = text.replace("\xF0\x9F\x98\xAB", "tired ")
+    text = text.replace("\xF0\x9F\x98\xAD", "crying ")
+    text = text.replace("\xF0\x9F\x98\xB0", "nervous ")
+    text = text.replace("\xF0\x9F\x98\xB1", "screaming ")
+    text = text.replace("\xF0\x9F\x98\xB2", "astonished ")
+    text = text.replace("\xF0\x9F\x98\xB3", "flushed ")
+    text = text.replace("\xF0\x9F\x98\xB5", "dizzy ")
+    text = text.replace("\xF0\x9F\x98\xB7", "mask ")
+
+    return text
+
+
+def interate_data(username):
+    editedTweets = []
+    data = pd.read_csv(username + "_tweets.csv", sep='__;_', engine='python')
+    for i in range(len(data.text)):
+        editedTweets.append([data.id[i], data.date[i], edit_text(data.text[i])])
+
+
 def main():
     startDate = datetime.datetime(2020, 6, 1, 0, 0, 0)
     endDate = datetime.datetime(2021, 1, 1, 0, 0, 0)
+    username = "BlazPlestenjak"
     # generates csv file for this user between startDate and endDate
-    get_twitter_data("BlazPlestenjak", startDate, endDate)
+    get_twitter_data(username, startDate, endDate)
+    interate_data(username)
 
 
 if __name__ == "__main__":
